07a7370c71acfbcf26b89c247ef25cff06950a47
Azat <azatalex@yahoo.com>
2019-03-05 10:46:25 +0100

    test

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 test=pd.read_csv("C:/Users/azata/OneDrive/Desktop/test.csv")
-test=pd.read_csv("C:/Users/azata/OneDrive/Desktop/test.csv")
+train=pd.read_csv("C:/Users/azata/OneDrive/Desktop/train.csv")
+sample=pd.read_csv("C:/Users/azata/OneDrive/Desktop/sample_submission.csv")
 
-
-asdasdasd
+print(test)
