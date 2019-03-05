3025851bf4663a454584f2330754c123bfa8335a
Lucas91380 <lucas.chabeau@gmail.com>
2019-03-05 11:09:29 +0100

    fichier d'importation

--- import.py
+++ import.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-test=pd.read_csv("data/test.csv")
-train=pd.read_csv("data/train.csv")
-sample=pd.read_csv("data/sample_submission.csv")
+test = pd.read_csv("data/test.csv")
+train = pd.read_csv("data/train.csv")
+sample = pd.read_csv("data/sample_submission.csv")
 
 print(test)
