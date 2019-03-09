ce774edd126504197723d1827519468b2c5a9894
MatFrancois <matthieu.francois@yahoo.fr>
2019-03-09 17:05:36 +0100

    test

--- modele.py
+++ modele.py
@@ -5,9 +5,15 @@ from sklearn.linear_model import LinearRegression
 
 #Reg
 modeleReg=LinearRegression()
-noms = train.columns.drop["revenue"]
-X = train[noms]
-y =  train.revenue
+noms = trainSamp.columns.drop(["id","title","cast","crew","revenue"])
 
+nomsquanti = noms.drop(["original_language","original_title","production_companies","release_date"])
+X = trainSamp[nomsquanti]
+y =  trainSamp.revenue
+
+tt = X.isna().sum()>0
+tt
+
+
+X.columns[]
 modeleReg.fit(X,y)
-#pushhh
